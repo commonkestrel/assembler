ed7265b4f686aa07978a2ea9435d4dbe153a9959
Kestrel <jettb6@gmail.com>
2023-07-22 23:10:04 -0700

    Start variables

--- examples/blink.asm
+++ examples/blink.asm
@@ -1,6 +1,9 @@
 // Not for actual use, but is inserted into every input.
 #include <core.asm>
 
+pub define WIDTH = 10
+const 
+
 /// This is called
 #reset:
      // Write bit 5 on the expansion port high.
